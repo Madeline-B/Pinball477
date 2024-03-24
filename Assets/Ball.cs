152c4568b27f0fab60824dc4d596d1e630ae67f0
David Olivier <deotrenton@gmail.com>
2024-03-23 22:24:10 -0500

    disable 'flight' of the pinball, send the pinball back to the beginning when it passes the flippers

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -6,6 +6,14 @@ public class Ball : MonoBehaviour
 {
 	public GameObject Pinball;
 	private Rigidbody rb;
+	public GameObject BallSensor;
+	public bool ballLaunched = false;
+	
+	public void OnTriggerEnter(Collider other){
+		ballLaunched = false;
+		Pinball.transform.position = new Vector3(-0.974f,0.807f,-4.475f);
+	}
+	
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +23,9 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space) && ballLaunched == false) {
 			rb.AddForce(new Vector3(0,0,-10), ForceMode.Impulse);
+			ballLaunched = true;
 		}
     }
 }
---
+++ Assets/LeftFlip.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftFlip : MonoBehaviour
+{
+	public GameObject LeftFlipper;
+	public float resting = 0f;
+	public float dampen = 150f;
+	public float strength = 10000f;
+	public float pressed = 45f;
+	HingeJoint hinge;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+		hinge.useSpring = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		JointSpring spring = new JointSpring();
+		spring.spring = strength;
+		spring.damper = dampen;
+		
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+			spring.targetPosition = pressed;
+		}else{
+			spring.targetPosition = resting;
+		}
+		
+		hinge.spring = spring;
+		hinge.useLimits = true;
+    }
+}
