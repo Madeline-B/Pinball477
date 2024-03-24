eea5471d841c509091bb95fff9f7acd86ce7de3d
David Olivier <deotrenton@gmail.com>
2024-03-24 12:03:25 -0500

    Limited number of balls, restart available, Game over Functional

--- Assets/GameProcess.cs
+++ Assets/GameProcess.cs
@@ -6,16 +6,23 @@ public class GameProcess : MonoBehaviour
 {
 	public GameObject MainMenu;
 	public GameObject GameOver;
+	public GameObject Pinball;
 	
 	public void StartGameClicked(){
 		MainMenu.SetActive(false);
+		Pinball.SetActive(true);
+	}
+	
+	public void RestartClicked(){
+		MainMenu.SetActive(true);
+		GameOver.SetActive(false);
 	}
 	
     // Start is called before the first frame update
     void Start()
     {
         GameOver.SetActive(false);
-		
+		Pinball.SetActive(false);
     }
 
     // Update is called once per frame
