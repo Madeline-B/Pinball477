05b8e93d890bcc91636651e5a83158c253faf71d
David Olivier <deotrenton@gmail.com>
2024-03-26 14:05:25 -0500

    Scoring system works, Your score, High Score carries over from restart

--- Assets/BallNumber.cs
+++ Assets/BallNumber.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class BallNumber : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class BallNumber : MonoBehaviour
 	// Start is called before the first frame update
     void Start()
     {
-     
+		
     }
 
     // Update is called once per frame
--- Assets/ScoreSynch.cs
+++ Assets/ScoreSynch.cs
@@ -7,21 +7,29 @@ public class ScoreSynch : MonoBehaviour
 {
 	public int HighScore = 0;
 	public GameObject Score;
-	public GameObject PlayerScore;
+	public GameObject PScore;
+	public GameObject HScore;
 	public TMP_Text txt;
 	public TMP_Text ptxt;
+	public TMP_Text htxt;
 	
     // Start is called before the first frame update
     void Start()
     {
         txt = Score.GetComponent<TextMeshPro>();
-
+		
 	}
 
     // Update is called once per frame
     void Update()
     {
-		ptxt = PlayerScore.GetComponent<TextMeshPro>();
+		ptxt = PScore.GetComponent<TextMeshProUGUI>();
         ptxt.text = txt.text;
+		htxt = HScore.GetComponent<TextMeshProUGUI>();
+		if(int.Parse(ptxt.text) > int.Parse(htxt.text)){
+			htxt = HScore.GetComponent<TextMeshProUGUI>();
+			htxt.text = txt.text;			
+		}
+
     }
 }
